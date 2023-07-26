e007b3e5e02d477ac85d8534c38152ef6f42a867
Bouchtarate <hamza.bouchtarate@gmail.com>
2023-07-26 23:57:04 +0100

    fix a bug

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 let player1 = document.querySelector(".player-1");
 let totalScore1 = document.getElementById("score-1");
 let currentScore1 = document.getElementById("current-1");
+let playerName1 = document.getElementById("name-1");
 let player2 = document.querySelector(".player-2");
 let totalScore2 = document.getElementById("score-2");
 let currentScore2 = document.getElementById("current-2");
-
+let playerName2 = document.getElementById("name-2");
 let newGameBtn = document.querySelector(".btn-new");
 let rollDiceBtn = document.querySelector(".btn-roll");
 let holdScoreBtn = document.querySelector(".btn-hold");
@@ -44,6 +45,7 @@ const holdScore = () => {
     player2.classList.add("player-active");
     if (totalScore1.textContent >= 100) {
       player1.classList.add("player-winner");
+      playerName1.textContent = "Player 1 win";
       holdScoreBtn.disabled = true;
       rollDiceBtn.disabled = true;
     }
@@ -53,8 +55,9 @@ const holdScore = () => {
     currentScore2.textContent = 0;
     player2.classList.remove("player-active");
     player1.classList.add("player-active");
-    if (totalScore1.textContent >= 100) {
-      player1.classList.add("player-winner");
+    if (totalScore2.textContent >= 100) {
+      player2.classList.add("player-winner");
+      playerName2.textContent = "Player 2 win";
       holdScoreBtn.disabled = true;
       rollDiceBtn.disabled = true;
     }
@@ -62,6 +65,10 @@ const holdScore = () => {
 };
 
 const playAgain = () => {
+  player1.classList.remove("player-winner");
+  player2.classList.remove("player-winner");
+  holdScoreBtn.disabled = false;
+  rollDiceBtn.disabled = false;
   player1.classList.add("player-active");
   player2.classList.remove("player-active");
   currentScore1.textContent = 0;
